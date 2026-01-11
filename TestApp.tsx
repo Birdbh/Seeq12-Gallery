f2bdd57dfc94e397805517a7fcf228f422e545ff
Brody Bird <birdlkjlkj123@gmail.com>
2026-01-11 16:26:28 -0500

    init

---
+++ TestApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestApp: React.FC = () => {
+  return (
+    <div style={{ padding: '20px', backgroundColor: 'lightblue', minHeight: '100vh' }}>
+      <h1 style={{ color: 'darkblue' }}>🎉 React is Working!</h1>
+      <p>If you can see this, the basic setup is correct.</p>
+    </div>
+  );
+};
+
+export default TestApp;
